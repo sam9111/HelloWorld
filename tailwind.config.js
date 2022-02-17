973c5694d8a78cddc5e16290d574ed54ead0a743
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-02-17 22:09:43 +0530

    add headless ui, loaders, inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-  content: [
-     "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
