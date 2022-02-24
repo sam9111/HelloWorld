6be737432f3090a8d34271b41c9f3d7c82ad6ec5
sam9111 <samyuktha19095@cse.ssn.edu.in>
2022-02-24 16:16:37 +0530

    added tooltip

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,6 @@
 export default function SideBar() {
   return (
-    <div className="flex justify-center  m-10 ">
+    <div className="flex justify-center m-10">
       <h1 className="text-8xl font-bold">Hello World</h1>
     </div>
   );
--- src/components/World.js
+++ src/components/World.js
@@ -18,11 +18,16 @@ export default function World() {
       <SideBar />
       <div className="w-2/3">
         <Globe
+          width={1000}
           globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
           backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-          pointAltitude="size"
-          pointColor="color"
+          pointAltitude={(p) => p.size / 4}
           pointsData={points}
+          pointRadius={0.75}
+          pointColor="color"
+          pointLabel={(p) =>
+            `${p.country} is majorly reporting ${p.sentiment} news`
+          }
         />
       </div>
     </div>
